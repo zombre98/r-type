2d35715395408c6cdb137619d91872974df196f5
Valkyrihane <anatole.juge@epitech.eu>
2018-11-07 16:34:02 +0100

    Add conan file and SFML (#2)
    * Add Conanfile to the project
    
    * remove sfml and add bincrafter
    
    * Remove sfml package options from connan install file
    
    * Add sfml compilation
    
    * Fix conanfile
    
    * [ADD] SFML sources in order to build it when compiling
    
    * [DEL] useless md file in SFML/
    
    * [DEL] useless examples in SFML/
    
    * [DEL] doc/ in SFML/
    
    * [DEL] useless .github files in SFML/

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,33 @@
 // Created by armandmgt on 11/5/18.
 //
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+	// run the program as long as the window is open
+	while (window.isOpen())
+	{
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// clear the window with black color
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
+	}
+
 	return 0;
 }
