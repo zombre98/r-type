3d032c32cc74332c2034f9d3b33d0d5885e590dd
Le Juge <anatole.juge@epitech.eu>
2018-11-15 00:00:55 +0100

    [ADD] useful defines in order to easily add new components

--- assets/lua/test_system.lua
+++ assets/lua/test_system.lua
@@ -1,6 +1,9 @@
 function update(delta)
-   print(delta)
+   print("===LUA===")
+   print("setting comp velocity to 5.6")
    entities[1]:getComponentVelocity().x = 5.6
+   print("creating new pos component")
+   entities[1]:addComponentPosition(666.0, 45.0)
+   print("getting new pos Component x:", entities[1]:getComponentPosition().x)
+   print("===LUA END===")
 end
-
-print("test")
--- src/server/main.cpp
+++ src/server/main.cpp
@@ -6,17 +6,20 @@
 #include "LuaSystem.hpp"
 
 int main(int argc, char *argv[]) {
+  //creating an entityVector with a default Velocity component
   ecs::entityVector v = std::make_shared<std::vector<ecs::entityPtr>>();
   v->push_back(std::make_unique<ecs::Entity>());
   (*v)[0]->addComponent<ecs::Velocity>(1.5f, 4.f);
 
-  std::cout << (*v)[0]->getComponent<ecs::Velocity>().x << std::endl;
+  std::cout << "Base velocity component x: " << (*v)[0]->getComponent<ecs::Velocity>().x << std::endl;
 
+  // creating LuaSystem with the entityVector and the file name (will check in assets/lua)
   ecs::LuaSystem testSystem(v, "test_system.lua");
-
+  //updating the system with a custom delta
   testSystem.update(102);
 
-  std::cout << (*v)[0]->getComponent<ecs::Velocity>().x << std::endl;
+  std::cout << "Checking velocity x changed (should be 5.6):" << (*v)[0]->getComponent<ecs::Velocity>().x << std::endl;
+
   if (argc == 2) {
 	std::cout << "Server started on port: " << argv[1] << std::endl;
   }
