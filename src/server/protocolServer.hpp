ae4b3a31a0febd2b7d1be15302a0456b8ec34e24
zombre <thomas.burgaud@epitech.eu>
2018-11-16 12:41:04 +0100

    Remove warning and add a static assert to check if the package we trying to send is base of Package

--- src/server/protocolServer.hpp
+++ src/server/protocolServer.hpp
@@ -8,6 +8,7 @@
 #include <boost/bind.hpp>
 #include <iostream>
 #include "server.hpp"
+#include "protocol.hpp"
 
 namespace net {
 	class protocolServer : public server {
@@ -23,12 +24,13 @@ namespace net {
 		}
 
 		template<typename T>
-		void doReceive(boost::shared_ptr<boost::array<T, 1>> value)
+		void doReceive(boost::shared_ptr<boost::array<T, 1>> &)
 		{
 		}
 
 		template<typename T>
 		void sendData(T data) {
+			static_assert(std::is_base_of<Package, T>(), "Data you trying to send is not base of Package struct");
 			boost::array<T, 1> newData = {{data}};
 			boost::shared_ptr<boost::array<T, 1>> dataToSend = boost::make_shared<boost::array<T, 1>>(newData);
 			std::cout << "Before sending : " << dataToSend->at(0).x << std::endl;
