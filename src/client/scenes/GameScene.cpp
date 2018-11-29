1354572146f6118b3979e881e38f68f3c0ca87d4
Valkyrihane <anatole.juge@epitech.eu>
2018-11-29 20:51:46 +0100

    Moving starfield (#31)
    * [ADD] starfield based on timer
    
    * [ADD] BG_FPS constant

--- src/client/scenes/GameScene.cpp
+++ src/client/scenes/GameScene.cpp
@@ -12,6 +12,11 @@ void GameScene::enter() noexcept {
 	_evtMgr.subscribe<net::NetPlayer>(*this);
 	_evtMgr.subscribe<net::Pos>(*this);
 	_resourceMgr.loadTexture("background.png");
+        _bg.setTexture(_resourceMgr.getTexture("background"));
+        _bg.scale(static_cast<float>(_parent.getWindow().getSize().x) /
+                  _bg.getTexture()->getSize().x,
+                  static_cast<float>(_parent.getWindow().getSize().y) /
+                  _bg.getTexture()->getSize().y);
 	_resourceMgr.loadAllTexturesInDirectory("");
 }
 
@@ -21,7 +26,7 @@ void GameScene::update(float timeSinceLastFrame) noexcept {
 
 void GameScene::displayGame(float timeSinceLastFrame[[maybe_unused]]) noexcept {
 	auto &window = _parent.getWindow();
-	//	window.draw(sf::Sprite{_resourceMgr.getTexture("background")});
+        _displayBg(window);
 	for (auto &it : _sprites) {
 		window.draw(it.second);
 	}
@@ -63,4 +68,26 @@ void GameScene::receive(const net::NetPlayer &player) {
 
 void GameScene::receive(const net::Pos &pos) {
 	_sprites.at(pos.head.id).setPosition(pos.x, pos.y);
+}
+
+void GameScene::_displayBg(sf::RenderWindow &window) {
+    static std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+    auto t2 = std::chrono::steady_clock::now();
+    auto timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+
+    window.draw(_bg);
+    auto bgExtension = sf::Sprite{*(_bg.getTexture())};
+    bgExtension.setScale(_bg.getScale());
+    bgExtension.setPosition(_bg.getPosition().x + _bg.getTexture()->getSize().x * _bg.getScale().x,
+                            _bg.getPosition().y);
+    window.draw(bgExtension);
+
+    if (timeSpan.count() < 1.f / BG_FPS)
+        return;
+
+    t1 = std::chrono::steady_clock::now();
+
+    _bg.setPosition(_bg.getPosition().x - 2, _bg.getPosition().y);
+    if (_bg.getPosition().x + _bg.getTexture()->getSize().x * _bg.getScale().x <= 0)
+        _bg.setPosition(0, _bg.getPosition().y);
 }
